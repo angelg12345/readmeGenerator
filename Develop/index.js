ec592c05e78faf7d46b10394fe8130ce62d67b0e
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-05-22 13:44:32 -0700

    added questions for the user input

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,30 @@ const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkdown');
 const path = require('path');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?'
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'write a short description of your project'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message:'which kind of license would you like?',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
